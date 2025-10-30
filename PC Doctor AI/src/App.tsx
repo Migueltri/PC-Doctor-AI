78f6ef180ee712d86f1c7ef5f374c8de68af3a14
Migueltri <mitrijurrea@gmail.com>
2025-10-30 15:19:27 +0100

    Update App.tsx

--- PC Doctor AI/src/App.tsx
+++ PC Doctor AI/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
+import home from './pages/home';
+import contact from './pages/contact';
 
 export default function App() {
   return (
