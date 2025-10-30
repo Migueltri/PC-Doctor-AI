71b83c5f0d2ea11e305e3c8c65ae483a343b4a98
Migueltri <mitrijurrea@gmail.com>
2025-10-30 15:39:17 +0100

    Update App.tsx

--- PC Doctor AI/src/App.tsx
+++ PC Doctor AI/src/App.tsx
@@ -1,14 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home/page";
 import ContactSection from "./pages/components/ContactSection";
+import AboutSection from "./pages/components/AboutSection";
+import HeroSection from "./pages/components/HeroSection";
+import ServicesSection from "./pages/components/ServicesSection";
+import NewsletterSection from "./pages/components/NewsletterSection";
+import ProductsSection from "./pages/components/ProductsSection";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <ProductsSection />
+      <NewsletterSection />
+      <ContactSection />
+      <Home />
+    </>
   );
 }
