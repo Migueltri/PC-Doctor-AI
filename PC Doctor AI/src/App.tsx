7b2217055328a38ecc74dea316cf3848bd77544a
Migueltri <mitrijurrea@gmail.com>
2025-10-30 14:21:30 +0100

    Update App.tsx

--- PC Doctor AI/src/App.tsx
+++ PC Doctor AI/src/App.tsx
@@ -1,15 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
-
-
-function App() {
-  return (
-    <BrowserRouter basename={__BASE_PATH__}>
-      <AppRoutes />
-    </BrowserRouter>
-  )
-}
-
 export default function App() {
   return (
     <h1>Hola desde PC Doctor AI</h1>
