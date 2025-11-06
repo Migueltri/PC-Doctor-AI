a6f4c50d8613a681123bd5680fd08dcf1886d0c1
Migueltri <mitrijurrea@gmail.com>
2025-11-06 10:17:07 +0100

    Update App.tsx

--- PC Doctor AI/src/App.tsx
+++ PC Doctor AI/src/App.tsx
@@ -5,15 +5,23 @@ import ProductsSection from "./pages/home/components/ProductsSection";
 import NewsletterSection from "./pages/home/components/NewsletterSection";
 import ContactSection from "./pages/home/components/ContactSection";
 
+import PCDoctorAI from "./components/ai/PCDoctorAI";
+import AppointmentScheduler from "./components/ai/AppointmentScheduler";
+
 export default function App() {
   return (
     <>
+      {/* Secciones actuales */}
       <HeroSection />
       <AboutSection />
       <ServicesSection />
       <ProductsSection />
       <NewsletterSection />
       <ContactSection />
+
+      {/* 🔹Nueva sección IA*/}
+      <PCDoctorAI />
+      <AppointmentScheduler />
     </>
   );
 }
